de76612fa1b0f40248ea0b0c5768cdb541e68664
Raul Ferreira <raulmendesferreira@gmail.com>
2018-04-20 17:53:19 +0100

    Make it more like the others.

--- data/c/c.py
+++ data/c/c.py
@@ -3,6 +3,7 @@ import sys
 import random
 
 dataName="c.dat"
+#dataName="c.mtx"
 file=open(dataName,"w+")
 m=1024000
 n=m
@@ -10,9 +11,6 @@ uid=0
 iid=0
 score=0
 print "start."
-#file.write("%%MatrixMarket matrix coordinate real general\n")
-#head = str(m) + "\t" + str(n) + "\t" + str(2*m) + "\n"
-#file.write(head)
 
 print "Writing the meta file..."
 meta=open("meta", "w+")
@@ -20,6 +18,10 @@ meta.write(str(m)+"\t"+str(n)+"\n")
 meta.write(str(2*m)+"\t"+dataName+"\n") 
 meta.close();
 print "Done."
+
+#file.write("%%MatrixMarket matrix coordinate real general\n")
+#head = str(m) + "\t" + str(n) + "\t" + str(2*m) + "\n"
+#file.write(head)
 
 for x in range(m): # generate two entries for each row
 	uid = x+1
