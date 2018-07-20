8d6b31f9ea78424c09ca571b1fd6abe28c4e0615
Raul Ferreira <raulmendesferreira@gmail.com>
2018-07-20 22:57:30 +0100

    Cosmetic changes.

--- src/util.cpp
+++ src/util.cpp
@@ -11,110 +11,99 @@
 using namespace std;
 
 // load utility for CCS RCS
-void load(const char* srcdir, smat_t &R, bool ifALS, bool with_weights)
-{
+void load(const char* srcdir, smat_t& R, bool ifALS, bool with_weights) {
     char filename[1024], buf[1024];
-    sprintf(filename,"%s/meta",srcdir);
-    FILE *fp = fopen(filename,"r");
+    sprintf(filename, "%s/meta", srcdir);
+    FILE* fp = fopen(filename, "r");
     unsigned m, n, nnz;
     fscanf(fp, "%u %u", &m, &n);
     fscanf(fp, "%u %s", &nnz, buf);
-    sprintf(filename,"%s/%s", srcdir, buf);
+    sprintf(filename, "%s/%s", srcdir, buf);
     R.load(m, n, nnz, filename, ifALS, with_weights);
     fclose(fp);
-    return ;
+    return;
 }
 
-
-int convertToString(const char *filename,string &s)
-{
+int convertToString(const char* filename, string& s) {
     size_t size;
     char* str;
-    fstream f(filename,(fstream::in|fstream::binary));
+    fstream f(filename, (fstream::in | fstream::binary));
 
-    if(f.is_open())
-    {
+    if (f.is_open()) {
         size_t fileSize;
-        f.seekg(0,fstream::end);
-        size=fileSize=(size_t)f.tellg();
-        f.seekg(0,fstream::beg);
-        str=new char[size+1];
-        if(!str)
-        {
+        f.seekg(0, fstream::end);
+        size = fileSize = (size_t) f.tellg();
+        f.seekg(0, fstream::beg);
+        str = new char[size + 1];
+        if (!str) {
             f.close();
             return 0;
         }
-        f.read(str,fileSize);
+        f.read(str, fileSize);
         f.close();
-        str[size]='\0';
-        s=str;
+        str[size] = '\0';
+        s = str;
         delete[] str;
         return 0;
     }
-    cout<<"Error:failed to open file:"<<filename<<"\n";
+    cout << "Error:failed to open file:" << filename << "\n";
     return -1;
 }
 
-int getPlatform(cl_platform_id &platform, int id)
-{
-    platform= nullptr;
+int getPlatform(cl_platform_id& platform, int id) {
+    platform = nullptr;
     cl_uint numPlatforms;
-    cl_int status=clGetPlatformIDs(0, nullptr,&numPlatforms);
-    if(status!=CL_SUCCESS)
-    {
-        cout<<"ERROR:Getting platforms!\n";
+    cl_int status = clGetPlatformIDs(0, nullptr, &numPlatforms);
+    if (status != CL_SUCCESS) {
+        cout << "ERROR:Getting platforms!\n";
         return -1;
     }
-    if(numPlatforms>0)
-    {
-        cl_platform_id* platforms=(cl_platform_id*)malloc(numPlatforms*sizeof(cl_platform_id));
-        status=clGetPlatformIDs(numPlatforms,platforms, nullptr);
-        if(status!=CL_SUCCESS)
-        {
-            cout<<"ERROR:Getting platform IDs!\n";
+    if (numPlatforms > 0) {
+        cl_platform_id* platforms = (cl_platform_id*) malloc(numPlatforms * sizeof(cl_platform_id));
+        status = clGetPlatformIDs(numPlatforms, platforms, nullptr);
+        if (status != CL_SUCCESS) {
+            cout << "ERROR:Getting platform IDs!\n";
             return -1;
         }
-        platform=platforms[id];
+        platform = platforms[id];
         free(platforms);
         return 0;
-    }
-    else
+    } else {
         return -1;
+    }
 }
 
-cl_device_id *getCl_device_id(cl_platform_id &platform, char* device_type)
-{
-    cl_uint numDevices=0;
+cl_device_id* getCl_device_id(cl_platform_id& platform, char* device_type) {
+    cl_uint numDevices = 0;
     cl_int status = 0;
-    cl_device_id *devices= nullptr;
+    cl_device_id* devices = nullptr;
 
-    if((device_type[0]=='m')&&(device_type[1]=='i')&&(device_type[2]=='c'))
-        status=clGetDeviceIDs(platform,CL_DEVICE_TYPE_ACCELERATOR,0, nullptr,&numDevices);
-    else if((device_type[0]=='c')&&(device_type[1]=='p')&&(device_type[2]=='u'))
-        status=clGetDeviceIDs(platform,CL_DEVICE_TYPE_CPU,0, nullptr,&numDevices);
-    else if((device_type[0]=='g')&&(device_type[1]=='p')&&(device_type[2]=='u'))
-        status=clGetDeviceIDs(platform,CL_DEVICE_TYPE_GPU,0, nullptr,&numDevices);
+    if ((device_type[0] == 'm') && (device_type[1] == 'i') && (device_type[2] == 'c')) {
+        status = clGetDeviceIDs(platform, CL_DEVICE_TYPE_ACCELERATOR, 0, nullptr, &numDevices);
+    } else if ((device_type[0] == 'c') && (device_type[1] == 'p') && (device_type[2] == 'u')) {
+        status = clGetDeviceIDs(platform, CL_DEVICE_TYPE_CPU, 0, nullptr, &numDevices);
+    } else if ((device_type[0] == 'g') && (device_type[1] == 'p') && (device_type[2] == 'u')) {
+        status = clGetDeviceIDs(platform, CL_DEVICE_TYPE_GPU, 0, nullptr, &numDevices);
+    }
 
-    if(status!=CL_SUCCESS)
-    {
-        cout<<"ERROR:Getting numDevices from clGetDeviceIDs!\n";
+    if (status != CL_SUCCESS) {
+        cout << "ERROR:Getting numDevices from clGetDeviceIDs!\n";
         exit(1);
     }
 
-    if(numDevices>0)
-    {
-        devices=(cl_device_id*)malloc(numDevices*sizeof(cl_device_id));
-        if((device_type[0]=='c')&&(device_type[1]=='p')&&(device_type[2]=='u'))
-            status=clGetDeviceIDs(platform,CL_DEVICE_TYPE_CPU,numDevices,devices, nullptr);
-        else if((device_type[0]=='m')&&(device_type[1]=='i')&&(device_type[2]=='c'))
-            status=clGetDeviceIDs(platform,CL_DEVICE_TYPE_ACCELERATOR,numDevices,devices, nullptr);
-        else if((device_type[0]=='g')&&(device_type[1]=='p')&&(device_type[2]=='u'))
-            status=clGetDeviceIDs(platform,CL_DEVICE_TYPE_GPU,numDevices,devices, nullptr);
+    if (numDevices > 0) {
+        devices = (cl_device_id*) malloc(numDevices * sizeof(cl_device_id));
+        if ((device_type[0] == 'c') && (device_type[1] == 'p') && (device_type[2] == 'u')) {
+            status = clGetDeviceIDs(platform, CL_DEVICE_TYPE_CPU, numDevices, devices, nullptr);
+        } else if ((device_type[0] == 'm') && (device_type[1] == 'i') && (device_type[2] == 'c')) {
+            status = clGetDeviceIDs(platform, CL_DEVICE_TYPE_ACCELERATOR, numDevices, devices, nullptr);
+        } else if ((device_type[0] == 'g') && (device_type[1] == 'p') && (device_type[2] == 'u')) {
+            status = clGetDeviceIDs(platform, CL_DEVICE_TYPE_GPU, numDevices, devices, nullptr);
+        }
     }
 
-    if(status!=CL_SUCCESS)
-    {
-        cout<<"ERROR:Getting devices from clGetDeviceIDs!\n";
+    if (status != CL_SUCCESS) {
+        cout << "ERROR:Getting devices from clGetDeviceIDs!\n";
         exit(1);
     }
     return devices;
