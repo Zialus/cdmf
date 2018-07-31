e5a89f15506e2d2bd28f9d132593efd1ab9a1632
Raul Ferreira <raulmendesferreira@gmail.com>
2018-07-31 13:09:42 +0100

    Fixed mixed indentation.

--- gpgpu.sh
+++ gpgpu.sh
@@ -17,14 +17,14 @@ rawA="dat.float.rmse"
 #echo "T=1:" >> ${rawA} 
 #for ds in ${DS[@]}; do
 #        for lws in ${LWS[@]}; do
-#		#./cdmf -k 40 -t 5 -T 5 -n 24 -l 0.05 -nThreadsPerBlock 512 -p 1 ./data/netflix/
-#		./cdmf -k 40 -t ${lws} -T 1 -P 0 -l 0.05 -nThreadsPerBlock 512 -p 1 ./data/${ds}/ > tmp.dat
+#               #./cdmf -k 40 -t 5 -T 5 -n 24 -l 0.05 -nThreadsPerBlock 512 -p 1 ./data/netflix/
+#               ./cdmf -k 40 -t ${lws} -T 1 -P 0 -l 0.05 -nThreadsPerBlock 512 -p 1 ./data/${ds}/ > tmp.dat
 #                #./cdmf -P 0 -nThreadsPerBlock $lws ../datasetcf/${ds} > tmp.dat
 #                echo -ne `grep "training time" ./tmp.dat | cut -d':' -f2 | cut -d' ' -f2` >> ${rawA}
 #                echo -ne "\t" >> ${rawA}
 #                echo -ne `grep "test RMSE" ./tmp.dat | cut -d'=' -f2 | cut -d' ' -f2` >> ${rawA}
 #                #echo -ne "\t" >> ${rawA}
-#        	echo "" >> ${rawA}
+#               echo "" >> ${rawA}
 #        done
 #        echo "" >> ${rawA}
 #done
@@ -32,14 +32,14 @@ rawA="dat.float.rmse"
 #echo "T=3:" >> ${rawA} 
 #for ds in ${DS[@]}; do
 #        for lws in ${LWS[@]}; do
-#		#./cdmf -k 40 -t 5 -T 5 -n 24 -l 0.05 -nThreadsPerBlock 512 -p 1 ./data/netflix/
-#		./cdmf -k 40 -t ${lws} -T 3 -P 0 -l 0.05 -nThreadsPerBlock 512 -p 1 ./data/${ds}/ > tmp.dat
+#               #./cdmf -k 40 -t 5 -T 5 -n 24 -l 0.05 -nThreadsPerBlock 512 -p 1 ./data/netflix/
+#               ./cdmf -k 40 -t ${lws} -T 3 -P 0 -l 0.05 -nThreadsPerBlock 512 -p 1 ./data/${ds}/ > tmp.dat
 #                #./cdmf -P 0 -nThreadsPerBlock $lws ../datasetcf/${ds} > tmp.dat
 #                echo -ne `grep "training time" ./tmp.dat | cut -d':' -f2 | cut -d' ' -f2` >> ${rawA}
 #                echo -ne "\t" >> ${rawA}
 #                echo -ne `grep "test RMSE" ./tmp.dat | cut -d'=' -f2 | cut -d' ' -f2` >> ${rawA}
 #                #echo -ne "\t" >> ${rawA}
-#        	echo "" >> ${rawA}
+#               echo "" >> ${rawA}
 #        done
 #        echo "" >> ${rawA}
 #done
@@ -47,14 +47,14 @@ rawA="dat.float.rmse"
 echo "T=5:" >> ${rawA} 
 for ds in ${DS[@]}; do
         for lws in ${LWS[@]}; do
-		#./cdmf -k 40 -t 5 -T 5 -n 24 -l 0.05 -nThreadsPerBlock 512 -p 1 ./data/netflix/
-		./cdmf -k 40 -t ${lws} -T 5 -P 0 -l 0.05 -nThreadsPerBlock 512 -p 1 ./data/${ds}/ > tmp.dat
+                #./cdmf -k 40 -t 5 -T 5 -n 24 -l 0.05 -nThreadsPerBlock 512 -p 1 ./data/netflix/
+                ./cdmf -k 40 -t ${lws} -T 5 -P 0 -l 0.05 -nThreadsPerBlock 512 -p 1 ./data/${ds}/ > tmp.dat
                 #./cdmf -P 0 -nThreadsPerBlock $lws ../datasetcf/${ds} > tmp.dat
                 echo -ne `grep "training time" ./tmp.dat | cut -d':' -f2 | cut -d' ' -f2` >> ${rawA}
                 echo -ne "\t" >> ${rawA}
                 echo -ne `grep "test RMSE" ./tmp.dat | cut -d'=' -f2 | cut -d' ' -f2` >> ${rawA}
                 #echo -ne "\t" >> ${rawA}
-        	echo "" >> ${rawA}
+                echo "" >> ${rawA}
         done
         echo "" >> ${rawA}
 done
@@ -62,14 +62,14 @@ done
 echo "T=7:" >> ${rawA} 
 for ds in ${DS[@]}; do
         for lws in ${LWS[@]}; do
-		#./cdmf -k 40 -t 5 -T 5 -n 24 -l 0.05 -nThreadsPerBlock 512 -p 1 ./data/netflix/
-		./cdmf -k 40 -t ${lws} -T 7 -P 0 -l 0.05 -nThreadsPerBlock 512 -p 1 ./data/${ds}/ > tmp.dat
+                #./cdmf -k 40 -t 5 -T 5 -n 24 -l 0.05 -nThreadsPerBlock 512 -p 1 ./data/netflix/
+                ./cdmf -k 40 -t ${lws} -T 7 -P 0 -l 0.05 -nThreadsPerBlock 512 -p 1 ./data/${ds}/ > tmp.dat
                 #./cdmf -P 0 -nThreadsPerBlock $lws ../datasetcf/${ds} > tmp.dat
                 echo -ne `grep "training time" ./tmp.dat | cut -d':' -f2 | cut -d' ' -f2` >> ${rawA}
                 echo -ne "\t" >> ${rawA}
                 echo -ne `grep "test RMSE" ./tmp.dat | cut -d'=' -f2 | cut -d' ' -f2` >> ${rawA}
                 #echo -ne "\t" >> ${rawA}
-        	echo "" >> ${rawA}
+                echo "" >> ${rawA}
         done
         echo "" >> ${rawA}
 done
@@ -85,9 +85,9 @@ fi
 echo "T=1:" >> ${rawB} 
 for ds in ${DS[@]}; do
         for lws in ${LWS[@]}; do
-		./cdmf.v3 -k 40 -t ${lws} -T 1 -P 0 -l 0.05 -nThreadsPerBlock 512 ./data/${ds}/ > tmp.dat
+                ./cdmf.v3 -k 40 -t ${lws} -T 1 -P 0 -l 0.05 -nThreadsPerBlock 512 ./data/${ds}/ > tmp.dat
                 echo -ne `grep "training time" ./tmp.dat | cut -d':' -f2 | cut -d' ' -f2` >> ${rawB}
-        	echo "" >> ${rawB}
+                echo "" >> ${rawB}
         done
         echo "" >> ${rawB}
 done
@@ -95,9 +95,9 @@ done
 echo "T=3:" >> ${rawB} 
 for ds in ${DS[@]}; do
         for lws in ${LWS[@]}; do
-		./cdmf.v3 -k 40 -t ${lws} -T 3 -P 0 -l 0.05 -nThreadsPerBlock 512 ./data/${ds}/ > tmp.dat
+                ./cdmf.v3 -k 40 -t ${lws} -T 3 -P 0 -l 0.05 -nThreadsPerBlock 512 ./data/${ds}/ > tmp.dat
                 echo -ne `grep "training time" ./tmp.dat | cut -d':' -f2 | cut -d' ' -f2` >> ${rawB}
-        	echo "" >> ${rawB}
+                echo "" >> ${rawB}
         done
         echo "" >> ${rawB}
 done
@@ -105,9 +105,9 @@ done
 echo "T=5:" >> ${rawB} 
 for ds in ${DS[@]}; do
         for lws in ${LWS[@]}; do
-		./cdmf.v3 -k 40 -t ${lws} -T 5 -P 0 -l 0.05 -nThreadsPerBlock 512 ./data/${ds}/ > tmp.dat
+                ./cdmf.v3 -k 40 -t ${lws} -T 5 -P 0 -l 0.05 -nThreadsPerBlock 512 ./data/${ds}/ > tmp.dat
                 echo -ne `grep "training time" ./tmp.dat | cut -d':' -f2 | cut -d' ' -f2` >> ${rawB}
-        	echo "" >> ${rawB}
+                echo "" >> ${rawB}
         done
         echo "" >> ${rawB}
 done
@@ -115,9 +115,9 @@ done
 echo "T=7:" >> ${rawB} 
 for ds in ${DS[@]}; do
         for lws in ${LWS[@]}; do
-		./cdmf.v3 -k 40 -t ${lws} -T 7 -P 0 -l 0.05 -nThreadsPerBlock 512 ./data/${ds}/ > tmp.dat
+                ./cdmf.v3 -k 40 -t ${lws} -T 7 -P 0 -l 0.05 -nThreadsPerBlock 512 ./data/${ds}/ > tmp.dat
                 echo -ne `grep "training time" ./tmp.dat | cut -d':' -f2 | cut -d' ' -f2` >> ${rawB}
-        	echo "" >> ${rawB}
+                echo "" >> ${rawB}
         done
         echo "" >> ${rawB}
 done
