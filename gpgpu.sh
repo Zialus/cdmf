e8a3c261486ac79c65fe9555f4c82a28e9b376a5
Raul Ferreira <raulmendesferreira@gmail.com>
2018-07-31 14:03:38 +0100

    Major cleanup to tests script.

--- gpgpu.sh
+++ gpgpu.sh
@@ -1,123 +1,53 @@
 #!/bin/bash
+set -x
 
 # ----- dataset loop begin -----
 
 #DS=(jester ml-20m netflix yahoor1)
-DS=(netflix)
+DS=(jester)
 #LWS=(1 2 3 4 5 6 7 8 9 10)
 LWS=(1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20)
+TS=(1 3 5 7)
 
-rawA="dat.float.rmse"
+rawA="dat.float.rmse.v2"
 
-#if [ -e ${rawA} ]
-#then
-#        rm ${rawA}
-#fi
+if [ -e ${rawA} ]
+then
+        rm ${rawA}
+fi
 
-#echo "T=1:" >> ${rawA} 
-#for ds in ${DS[@]}; do
-#        for lws in ${LWS[@]}; do
-#               #./cdmf -k 40 -t 5 -T 5 -n 24 -l 0.05 -nThreadsPerBlock 512 -p 1 ./data/netflix/
-#               ./cdmf -k 40 -t ${lws} -T 1 -P 0 -l 0.05 -nThreadsPerBlock 512 -p 1 ./data/${ds}/ > tmp.dat
-#                #./cdmf -P 0 -nThreadsPerBlock $lws ../datasetcf/${ds} > tmp.dat
-#                echo -ne `grep "training time" ./tmp.dat | cut -d':' -f2 | cut -d' ' -f2` >> ${rawA}
-#                echo -ne "\t" >> ${rawA}
-#                echo -ne `grep "test RMSE" ./tmp.dat | cut -d'=' -f2 | cut -d' ' -f2` >> ${rawA}
-#                #echo -ne "\t" >> ${rawA}
-#               echo "" >> ${rawA}
-#        done
-#        echo "" >> ${rawA}
-#done
-
-#echo "T=3:" >> ${rawA} 
-#for ds in ${DS[@]}; do
-#        for lws in ${LWS[@]}; do
-#               #./cdmf -k 40 -t 5 -T 5 -n 24 -l 0.05 -nThreadsPerBlock 512 -p 1 ./data/netflix/
-#               ./cdmf -k 40 -t ${lws} -T 3 -P 0 -l 0.05 -nThreadsPerBlock 512 -p 1 ./data/${ds}/ > tmp.dat
-#                #./cdmf -P 0 -nThreadsPerBlock $lws ../datasetcf/${ds} > tmp.dat
-#                echo -ne `grep "training time" ./tmp.dat | cut -d':' -f2 | cut -d' ' -f2` >> ${rawA}
-#                echo -ne "\t" >> ${rawA}
-#                echo -ne `grep "test RMSE" ./tmp.dat | cut -d'=' -f2 | cut -d' ' -f2` >> ${rawA}
-#                #echo -ne "\t" >> ${rawA}
-#               echo "" >> ${rawA}
-#        done
-#        echo "" >> ${rawA}
-#done
-
-echo "T=5:" >> ${rawA} 
-for ds in ${DS[@]}; do
-        for lws in ${LWS[@]}; do
-                #./cdmf -k 40 -t 5 -T 5 -n 24 -l 0.05 -nThreadsPerBlock 512 -p 1 ./data/netflix/
-                ./cdmf -k 40 -t ${lws} -T 5 -P 0 -l 0.05 -nThreadsPerBlock 512 -p 1 ./data/${ds}/ > tmp.dat
-                #./cdmf -P 0 -nThreadsPerBlock $lws ../datasetcf/${ds} > tmp.dat
-                echo -ne `grep "training time" ./tmp.dat | cut -d':' -f2 | cut -d' ' -f2` >> ${rawA}
-                echo -ne "\t" >> ${rawA}
-                echo -ne `grep "test RMSE" ./tmp.dat | cut -d'=' -f2 | cut -d' ' -f2` >> ${rawA}
-                #echo -ne "\t" >> ${rawA}
+for ts in ${TS[@]}; do
+        echo "T=${ts}:" >> ${rawA}
+        for ds in ${DS[@]}; do
+                for lws in ${LWS[@]}; do
+                        ./cdmf -k 40 -t ${lws} -T ${ts} -P 0 -l 0.05 -nThreadsPerBlock 256 -p 1 -V 2 ../data/${ds}/ > tmp.dat
+                        echo -ne `grep "training time" ./tmp.dat | cut -d':' -f2 | cut -d' ' -f2` >> ${rawA}
+                        echo -ne "\t" >> ${rawA}
+                        echo -ne `grep "test RMSE" ./tmp.dat | cut -d'=' -f2 | cut -d' ' -f2` >> ${rawA}
+                        echo "" >> ${rawA}
+                done
                 echo "" >> ${rawA}
         done
-        echo "" >> ${rawA}
-done
-
-echo "T=7:" >> ${rawA} 
-for ds in ${DS[@]}; do
-        for lws in ${LWS[@]}; do
-                #./cdmf -k 40 -t 5 -T 5 -n 24 -l 0.05 -nThreadsPerBlock 512 -p 1 ./data/netflix/
-                ./cdmf -k 40 -t ${lws} -T 7 -P 0 -l 0.05 -nThreadsPerBlock 512 -p 1 ./data/${ds}/ > tmp.dat
-                #./cdmf -P 0 -nThreadsPerBlock $lws ../datasetcf/${ds} > tmp.dat
-                echo -ne `grep "training time" ./tmp.dat | cut -d':' -f2 | cut -d' ' -f2` >> ${rawA}
-                echo -ne "\t" >> ${rawA}
-                echo -ne `grep "test RMSE" ./tmp.dat | cut -d'=' -f2 | cut -d' ' -f2` >> ${rawA}
-                #echo -ne "\t" >> ${rawA}
-                echo "" >> ${rawA}
-        done
-        echo "" >> ${rawA}
 done
 
 #-------------------------------------------------
-rawB="dat.float.rmse.time"
+rawB="dat.float.rmse.v3"
 
 if [ -e ${rawB} ]
 then
         rm ${rawB}
 fi
 
-echo "T=1:" >> ${rawB} 
-for ds in ${DS[@]}; do
-        for lws in ${LWS[@]}; do
-                ./cdmf.v3 -k 40 -t ${lws} -T 1 -P 0 -l 0.05 -nThreadsPerBlock 512 ./data/${ds}/ > tmp.dat
-                echo -ne `grep "training time" ./tmp.dat | cut -d':' -f2 | cut -d' ' -f2` >> ${rawB}
+for ts in ${TS[@]}; do
+        echo "T=${ts}:" >> ${rawB}
+        for ds in ${DS[@]}; do
+                for lws in ${LWS[@]}; do
+                        ./cdmf -k 40 -t ${lws} -T ${ts} -P 0 -l 0.05 -nThreadsPerBlock 256 -p 1 -V 3 ./data/${ds}/ > tmp.dat
+                        echo -ne `grep "training time" ./tmp.dat | cut -d':' -f2 | cut -d' ' -f2` >> ${rawB}
+                        echo "" >> ${rawB}
+                        echo -ne `grep "test RMSE" ./tmp.dat | cut -d'=' -f2 | cut -d' ' -f2` >> ${rawA}
+                        echo "" >> ${rawA}
+                done
                 echo "" >> ${rawB}
         done
-        echo "" >> ${rawB}
-done
-
-echo "T=3:" >> ${rawB} 
-for ds in ${DS[@]}; do
-        for lws in ${LWS[@]}; do
-                ./cdmf.v3 -k 40 -t ${lws} -T 3 -P 0 -l 0.05 -nThreadsPerBlock 512 ./data/${ds}/ > tmp.dat
-                echo -ne `grep "training time" ./tmp.dat | cut -d':' -f2 | cut -d' ' -f2` >> ${rawB}
-                echo "" >> ${rawB}
-        done
-        echo "" >> ${rawB}
-done
-
-echo "T=5:" >> ${rawB} 
-for ds in ${DS[@]}; do
-        for lws in ${LWS[@]}; do
-                ./cdmf.v3 -k 40 -t ${lws} -T 5 -P 0 -l 0.05 -nThreadsPerBlock 512 ./data/${ds}/ > tmp.dat
-                echo -ne `grep "training time" ./tmp.dat | cut -d':' -f2 | cut -d' ' -f2` >> ${rawB}
-                echo "" >> ${rawB}
-        done
-        echo "" >> ${rawB}
-done
-
-echo "T=7:" >> ${rawB} 
-for ds in ${DS[@]}; do
-        for lws in ${LWS[@]}; do
-                ./cdmf.v3 -k 40 -t ${lws} -T 7 -P 0 -l 0.05 -nThreadsPerBlock 512 ./data/${ds}/ > tmp.dat
-                echo -ne `grep "training time" ./tmp.dat | cut -d':' -f2 | cut -d' ' -f2` >> ${rawB}
-                echo "" >> ${rawB}
-        done
-        echo "" >> ${rawB}
 done
