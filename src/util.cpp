92f7377fb0186362401f42a32777f594b2eb2702
Raul Ferreira <raulmendesferreira@gmail.com>
2018-09-16 17:35:56 +0100

    Changed NULLs to nullptrs.

--- src/util.cpp
+++ src/util.cpp
@@ -66,44 +66,44 @@ void print_all_the_info() {
     for (i = 0; i < platformCount; i++) {
 
         // get all devices
-        clGetDeviceIDs(platforms[i], CL_DEVICE_TYPE_ALL, 0, NULL, &deviceCount);
+        clGetDeviceIDs(platforms[i], CL_DEVICE_TYPE_ALL, 0, nullptr, &deviceCount);
         devices = (cl_device_id*) malloc(sizeof(cl_device_id) * deviceCount);
-        clGetDeviceIDs(platforms[i], CL_DEVICE_TYPE_ALL, deviceCount, devices, NULL);
+        clGetDeviceIDs(platforms[i], CL_DEVICE_TYPE_ALL, deviceCount, devices, nullptr);
 
         // for each device print critical attributes
         for (j = 0; j < deviceCount; j++) {
 
             // print device name
-            clGetDeviceInfo(devices[j], CL_DEVICE_NAME, 0, NULL, &valueSize);
+            clGetDeviceInfo(devices[j], CL_DEVICE_NAME, 0, nullptr, &valueSize);
             value = (char*) malloc(valueSize);
-            clGetDeviceInfo(devices[j], CL_DEVICE_NAME, valueSize, value, NULL);
+            clGetDeviceInfo(devices[j], CL_DEVICE_NAME, valueSize, value, nullptr);
             printf("%d. Device: %s\n", j + 1, value);
             free(value);
 
             // print hardware device version
-            clGetDeviceInfo(devices[j], CL_DEVICE_VERSION, 0, NULL, &valueSize);
+            clGetDeviceInfo(devices[j], CL_DEVICE_VERSION, 0, nullptr, &valueSize);
             value = (char*) malloc(valueSize);
-            clGetDeviceInfo(devices[j], CL_DEVICE_VERSION, valueSize, value, NULL);
+            clGetDeviceInfo(devices[j], CL_DEVICE_VERSION, valueSize, value, nullptr);
             printf(" %d.%d Hardware version: %s\n", j + 1, 1, value);
             free(value);
 
             // print software driver version
-            clGetDeviceInfo(devices[j], CL_DRIVER_VERSION, 0, NULL, &valueSize);
+            clGetDeviceInfo(devices[j], CL_DRIVER_VERSION, 0, nullptr, &valueSize);
             value = (char*) malloc(valueSize);
-            clGetDeviceInfo(devices[j], CL_DRIVER_VERSION, valueSize, value, NULL);
+            clGetDeviceInfo(devices[j], CL_DRIVER_VERSION, valueSize, value, nullptr);
             printf(" %d.%d Software version: %s\n", j + 1, 2, value);
             free(value);
 
             // print c version supported by compiler for device
-            clGetDeviceInfo(devices[j], CL_DEVICE_OPENCL_C_VERSION, 0, NULL, &valueSize);
+            clGetDeviceInfo(devices[j], CL_DEVICE_OPENCL_C_VERSION, 0, nullptr, &valueSize);
             value = (char*) malloc(valueSize);
-            clGetDeviceInfo(devices[j], CL_DEVICE_OPENCL_C_VERSION, valueSize, value, NULL);
+            clGetDeviceInfo(devices[j], CL_DEVICE_OPENCL_C_VERSION, valueSize, value, nullptr);
             printf(" %d.%d OpenCL C version: %s\n", j + 1, 3, value);
             free(value);
 
             // print parallel compute units
-            clGetDeviceInfo(devices[j], CL_DEVICE_MAX_COMPUTE_UNITS,
-                            sizeof(maxComputeUnits), &maxComputeUnits, NULL);
+            clGetDeviceInfo(devices[j], CL_DEVICE_MAX_COMPUTE_UNITS, sizeof(maxComputeUnits), &maxComputeUnits,
+                            nullptr);
             printf(" %d.%d Parallel compute units: %d\n", j + 1, 4, maxComputeUnits);
 
         }
